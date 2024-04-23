65fdb837ac219c9ff6258c233bdaeae4a985b46a
ianthehamster <ianchow9898@gmail.com>
2024-04-23 18:49:46 +0800

    Added multiply unit tests

--- test/utils.js
+++ test/utils.js
@@ -1,26 +1,55 @@
-const { expect } = require("chai");
-const { add } = require("../utils.js");
+const { expect } = require('chai'); // 'expect' is an assertion syntax that helps us 'assert' our code meets expectations
+const { add, multiply } = require('../utils.js'); // importing our add function
+const { loopOverNumberArray } = require('../utils.js');
 
-describe("Utils", () => {
-  describe("Add", () => {
-    it("Adds 2 of the same number", () => {
+// 'describe' groups and categorises tests - accepts string (test description) and function (test logic) in arguments
+describe('Utils', () => {
+  // 'Utils' is description of all the tests in the outer arrow function
+  describe('Add', () => {
+    // 'Add' is description of the add function we are testing
+    it('Adds 2 of the same number', () => {
       const result = add(1, 1);
       expect(result).to.equal(2);
     });
 
-    it("Adds 2 different numbers", () => {
+    it('Adds 2 different numbers', () => {
       const result = add(1, 2);
       expect(result).to.equal(3);
     });
 
-    it("Adds a positive and a negative number", () => {
+    // adding an x in front of 'it' will skip this test
+    xit('Adds a positive and a negative number', () => {
       const result = add(1, -1);
       expect(result).to.equal(0);
     });
 
-    it("Adds 2 negative numbers", () => {
+    it('Adds 2 negative numbers', () => {
       const result = add(-1, -1);
       expect(result).to.equal(-2);
+    });
+  });
+
+  describe('Loop', () => {
+    it('Target is in array', () => {
+      const result = loopOverNumberArray([1, 2, 3], 2);
+      expect(result).to.equal(2);
+    });
+
+    it('Target is not in array', () => {
+      const result = loopOverNumberArray([1, 2, 3], 4);
+      expect(result).to.equal('Target not found');
+    });
+  });
+
+  describe('Multiply', () => {
+    it('Multiply 2 positive numbers', () => {
+      const result = multiply(2, 3);
+      expect(result).to.equal(6);
+    });
+
+    it('Multiply 1 positive and negative numbers', () => {
+      const result = multiply(5, -8);
+      expect(result).to.equal(-40);
     });
   });
 });
--- utils.js
+++ utils.js
@@ -2,6 +2,22 @@ const add = (a, b) => {
   return a + b;
 };
 
+const loopOverNumberArray = function (x, target) {
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] === target) {
+      return x[i];
+    }
+  }
+
+  return 'Target not found';
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
 module.exports = {
+  loopOverNumberArray,
   add,
+  multiply,
 };
